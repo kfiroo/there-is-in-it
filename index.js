e78e49cb27f5caf734e23fc1acfc3370adb4e799
kfiroo <kfirgolan@gmail.com>
2017-02-04 02:45:54 +0200

    inject Bot + remove token

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 
 const fs = require('fs');
 const _ = require('lodash');
+const Bot = require('slackbots');
 const runBot = require('./lib/runBot');
 
-const token = process.env.SLACK_BOT_API_TOKEN || 'xoxb-136937362866-yrrw0XR07ng7mln3wXSSGgPf';
+const token = process.env.SLACK_BOT_API_TOKEN;
 const name = process.env.SLACK_BOT_NAME || 'there_is_in_it';
 const wordsPath = './data/wordsThatHaveInThem.txt';
-
 
 fs.readFile(wordsPath, 'utf8', (err, data) => {
     if (err) {
         throw err;
     }
     const words = _.map(data.split(/\r?\n/), w => w.trim().toLowerCase().replace(/\s/g, ''));
-    runBot(token, name, words);
+    runBot(Bot, token, name, words);
 });
 
